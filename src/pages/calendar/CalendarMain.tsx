65343b2ff66eba4304bda3f1835bea6f003ab267
Razboy20 <razboy20@gmail.com>
2023-12-30 20:07:38 -0800

    initial calendar pages

---
+++ src/pages/calendar/CalendarMain.tsx
@@ -0,0 +1,10 @@
+import ExtensionRoot from '@src/views/components/common/ExtensionRoot/ExtensionRoot';
+import React from 'react';
+
+export default function CalendarMain() {
+    return (
+        <ExtensionRoot>
+            <div>Calendar Placeholder</div>
+        </ExtensionRoot>
+    );
+}
