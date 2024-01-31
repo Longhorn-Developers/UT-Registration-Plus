da9e7aac4135afa5184e4949c36a288ef6ab62a7
abhinavchadaga <abhinav.chadaga@utexas.edu>
2024-01-31 03:27:37 -0600

    feat: basic CalendarCourseMeeting component laid out - missing Text and Right Icon

---
+++ src/views/components/common/CalendarCourseMeeting/CalendarCourseMeeting.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Course } from 'src/shared/types/Course';
+import { CourseMeeting } from 'src/shared/types/CourseMeeting';
+import styles from './CalendarCourseMeeting.module.scss';
+
+/**
+ * Props for the CalendarCourseMeeting component.
+ */
+export interface CalendarCourseMeetingProps {
+    /** The Course that the meeting is for. */
+    course: Course;
+    /* index into course meeting array to display */
+    meetingIdx?: number;
+    /** The background color for the course. */
+    color: string;
+    /** The icon to display on the right side of the course. This is optional. */
+    rightIcon?: React.ReactNode;
+}
+
+/**
+ * `CalendarCourseMeeting` is a functional component that displays a course meeting.
+ *
+ * @example
+ * <CalendarCourseMeeting course={course} meeting={meeting} color="red" rightIcon={<Icon />} />
+ */
+const CalendarCourseMeeting: React.FC<CalendarCourseMeetingProps> = ({
+    course,
+    meetingIdx,
+}: CalendarCourseMeetingProps) => {
+    let meeting: CourseMeeting | null = meetingIdx !== undefined ? course.schedule.meetings[meetingIdx] : null;
+    return (
+        <div className={styles['calendar-course']}>
+            <div>
+                {course.department} {course.number} - {course.instructors[0].lastName}
+            </div>
+            {meeting && (
+                <div>
+                    {`${meeting.getTimeString({ separator: '-', capitalize: true })}${
+                        meeting.location ? ` - ${meeting.location.building}` : ''
+                    }`}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default CalendarCourseMeeting;
