3ff88d98fddbc0d437828ec3d4158865f33888e8
Abhinav Chadaga <abhinav.chadaga@utexas.edu>
2024-02-03 14:29:59 -0600

    use icons

---
+++ src/stories/components/CalendarCourseBlock.stories.tsx
@@ -0,0 +1,62 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Course, Status } from 'src/shared/types/Course';
+import { CourseMeeting, DAY_MAP } from 'src/shared/types/CourseMeeting';
+import { CourseSchedule } from 'src/shared/types/CourseSchedule';
+import Instructor from 'src/shared/types/Instructor';
+import CalendarCourse from 'src/views/components/common/CalendarCourseBlock/CalendarCourseBlock';
+
+const meta = {
+    title: 'Components/Common/CalendarCourseMeeting',
+    component: CalendarCourse,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        course: { control: 'object' },
+        meetingIdx: { control: 'number' },
+        color: { control: 'color' },
+        rightIcon: { control: 'object' },
+    },
+} satisfies Meta<typeof CalendarCourse>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        course: new Course({
+            uniqueId: 123,
+            number: '311C',
+            fullName: "311C - Bevo's Default Course",
+            courseName: "Bevo's Default Course",
+            department: 'BVO',
+            creditHours: 3,
+            status: Status.WAITLISTED,
+            instructors: [new Instructor({ firstName: '', lastName: 'Bevo', fullName: 'Bevo' })],
+            isReserved: false,
+            url: '',
+            flags: [],
+            schedule: new CourseSchedule({
+                meetings: [
+                    new CourseMeeting({
+                        days: [DAY_MAP.M, DAY_MAP.W, DAY_MAP.F],
+                        startTime: 480,
+                        endTime: 570,
+                        location: {
+                            building: 'UTC',
+                            room: '123',
+                        },
+                    }),
+                ],
+            }),
+            instructionMode: 'In Person',
+            semester: {
+                year: 2024,
+                season: 'Spring',
+            },
+        }),
+        meetingIdx: 0,
+        color: 'red',
+    },
+};
--- src/views/components/common/CalendarCourseBlock/CalendarCourseBlock.tsx
+++ src/views/components/common/CalendarCourseBlock/CalendarCourseBlock.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Course } from 'src/shared/types/Course';
+import { Course, Status } from 'src/shared/types/Course';
 import { CourseMeeting } from 'src/shared/types/CourseMeeting';
+import ClosedIcon from '~icons/material-symbols/lock';
+import WaitlistIcon from '~icons/material-symbols/timelapse';
+import CancelledIcon from '~icons/material-symbols/warning';
 import Text from '../Text/Text';
 import styles from './CalendarCourseBlock.module.scss';
 
@@ -15,11 +18,20 @@ export interface CalendarCourseBlockProps {
 
 const CalendarCourseBlock: React.FC<CalendarCourseBlockProps> = ({ course, meetingIdx }: CalendarCourseBlockProps) => {
     let meeting: CourseMeeting | null = meetingIdx !== undefined ? course.schedule.meetings[meetingIdx] : null;
+    let rightIcon: React.ReactNode | null = null;
+    if (course.status === Status.WAITLISTED) {
+        rightIcon = <WaitlistIcon />;
+    } else if (course.status === Status.CLOSED) {
+        rightIcon = <ClosedIcon />;
+    } else if (course.status === Status.CANCELLED) {
+        rightIcon = <CancelledIcon />;
+    }
+
     return (
         <div className={styles.component}>
             <div className={styles.content}>
                 <div className={styles['course-detail']}>
-                    <Text variant='h1-course'>
+                    <Text variant='h1-course' className={styles['course-title']}>
                         {course.department} {course.number} - {course.instructors[0].lastName}
                     </Text>
                     <Text variant='h3-course'>
@@ -28,6 +40,7 @@ const CalendarCourseBlock: React.FC<CalendarCourseBlockProps> = ({ course, meeti
                         }`}
                     </Text>
                 </div>
+                {rightIcon && <div className={styles['course-status']}>{rightIcon}</div>}
             </div>
         </div>
     );
