7cf503c65a9747f65e13d7d6fc01849b141d6a83
DhruvArora-03 <dhruvarora@utexas.edu>
2024-02-03 15:25:27 -0600

    uhhh made stuff

--- src/views/components/common/PopupCourseBlock/PopupCourseBlock.tsx
+++ src/views/components/common/PopupCourseBlock/PopupCourseBlock.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Course } from 'src/shared/types/Course';
+import classNames from 'classnames';
+import styles from './PopupCourseBlock.module.scss';
+import Text from '../Text/Text';
+import Icon from '../Icon/Icon';
 
 type PopupCourseBlockProps = {
     className?: string;
@@ -8,13 +12,18 @@ type PopupCourseBlockProps = {
 };
 
 /**
+ * The "course block" to be used in the extension popup.
  *
- * @param props
+ * @param props PopupCourseBlockProps
  */
-export default function PopupCourseBlock({ className, style, course }: PopupCourseBlockProps) {
+export default function PopupCourseBlock({ className, style, course }: PopupCourseBlockProps): JSX.Element {
     return (
-        <div className={className} style={style}>
-            {`${course.uniqueId} ${course.department} ${course.number} - ${course.instructors.map(v => v.lastName)}`}
+        <div className={classNames(className, styles.courseBlock)} style={style}>
+            <div className={styles.dragHandle}>⋮⋮</div>
+            <Text className={styles.courseInfo} variant='h1-course'>
+                {`${course.uniqueId} ${course.department} ${course.number} - ${course.instructors.map(v => v.lastName)}`}
+            </Text>
+            <Icon className={styles.icon} name='timelapse' />
         </div>
     );
 }
