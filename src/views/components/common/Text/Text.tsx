d102661e05cb423f479da90fa995383325915b79
Samuel Gunter <sgunter@utexas.edu>
2024-02-03 01:22:42 -0600

    feat: updated text component to v2 design
    wip

--- src/views/components/common/Text/Text.tsx
+++ src/views/components/common/Text/Text.tsx
@@ -1,23 +1,22 @@
 import classNames from 'classnames';
 import React, { PropsWithChildren } from 'react';
-import colors, { Color } from '@views/styles/colors.module.scss';
-import { Size, Weight } from '@views/styles/fonts.module.scss';
 import styles from './Text.module.scss';
 
 /**
  *
  */
 export type TextProps = {
-    color?: Color;
-    weight?: Weight;
-    size?: Size;
-    span?: boolean;
-    className?: string;
+    variant?: Variant;
     onClick?: () => void;
     title?: string;
     align?: React.CSSProperties['textAlign'];
     style?: React.CSSProperties;
+    className?: string;
 };
+
+const variants = ['mini', 'small', 'p', 'h4', 'h3-course', 'h3', 'h2-course', 'h2', 'h1-course', 'h1'] as const;
+
+type Variant = (typeof variants)[number];
 
 /**
  * A reusable Text component with props that build on top of the design system for the extension
@@ -26,25 +25,13 @@ export default function Text(props: PropsWithChildren<TextProps>) {
     const style: React.CSSProperties = {
         ...props.style,
         textAlign: props.align,
-        color: props.color ? colors[props.color] : undefined,
     };
 
-    const weightClass = `${props.weight ?? 'regular'}_weight`;
-    const fontSizeClass = `${props.size ?? 'medium'}_size`;
-
-    const className = classNames(styles.text, styles[weightClass], styles[fontSizeClass], props.className);
-
-    if (props.span) {
-        return (
-            <span title={props.title} className={className} style={style} onClick={props.onClick}>
-                {props.children}
-            </span>
-        );
-    }
+    const className = classNames(styles.text, styles[props.variant], props.className);
 
     return (
-        <div title={props.title} className={className} style={style} onClick={props.onClick}>
+        <span title={props.title} className={className} style={style} onClick={props.onClick}>
             {props.children}
-        </div>
+        </span>
     );
 }
