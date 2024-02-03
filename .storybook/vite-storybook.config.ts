c618b5e59463796c46b5956e0019fdd4a7da93cf
Razboy20 <razboy20@gmail.com>
2024-02-03 14:16:35 -0600

    hotfix: icon library resolution

--- .storybook/vite-storybook.config.ts
+++ .storybook/vite-storybook.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
+import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
 
 const root = resolve(__dirname, '../src');
@@ -11,16 +12,16 @@ console.log(root);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: root,
-      '@assets': assetsDir,
-      '@pages': pagesDir,
-      '@public': publicDir,
-      '@shared': resolve(root, 'shared'),
-      '@background': resolve(pagesDir, 'background'),
-      '@views': resolve(root, 'views'),
+    plugins: [react(), Icons({ compiler: 'jsx', jsx: 'react' })],
+    resolve: {
+        alias: {
+            src: root,
+            '@assets': assetsDir,
+            '@pages': pagesDir,
+            '@public': publicDir,
+            '@shared': resolve(root, 'shared'),
+            '@background': resolve(pagesDir, 'background'),
+            '@views': resolve(root, 'views'),
+        },
     },
-  },
 });
