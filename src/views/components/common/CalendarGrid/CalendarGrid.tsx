1bb6191244f40220a9af66cced4fe8f0c1e546a0
Derek Chen <derex1987@gmail.com>
2024-02-05 23:05:15 -0600

    feat: align day labels and add to grid

--- src/views/components/common/CalendarGrid/CalendarGrid.tsx
+++ src/views/components/common/CalendarGrid/CalendarGrid.tsx
@@ -21,16 +21,12 @@ const Calendar: React.FC = (props) => {
       <div className={styles.dayLabelContainer}>
         {/* Empty cell in the top-left corner */}
         <div className={styles.day} />
-        {/* Displaying day labels */}
-        {daysOfWeek.map(day => (
-          <div key={day} className={styles.day}>
-            {day}
-          </div>
-        ))}
+        
       </div>
       {/* Displaying the rest of the calendar */}
       <div className={styles.timeAndGrid}>
         <div className={styles.timeColumn}>
+          <div className={styles.timeBlock}></div>
           {hoursOfDay.map((hour) => (
             <div key={hour} className={styles.timeBlock}>
               <div className={styles.timeLabelContainer}>
@@ -40,6 +36,12 @@ const Calendar: React.FC = (props) => {
           ))}
         </div>
         <div className={styles.calendarGrid}>
+          {/* Displaying day labels */}
+          {daysOfWeek.map(day => (
+            <div key={day} className={styles.day}>
+              {day}
+            </div>
+          ))}
           {grid.map((row, rowIndex) => (
             row
           ))}
