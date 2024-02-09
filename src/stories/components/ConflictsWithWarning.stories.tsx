dc19d3975accb65c51c5b0fcbbaafad1f257db3a
knownotunknown <78577376+knownotunknown@users.noreply.github.com>
2024-02-09 10:53:41 -0600

    Implemented ConflictsWithWarning

---
+++ src/stories/components/ConflictsWithWarning.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/react';
+import ConflictsWithWarning from '@views/components/common/ConflictsWithWarning/ConflictsWithWarning';
+
+const meta = {
+    title: 'Components/Common/ConflictsWithWarning',
+    component: ConflictsWithWarning,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        ConflictingCourse: { control: 'string' },
+        SectionNumber: { control: 'string' },
+    },
+} satisfies Meta<typeof ConflictsWithWarning>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        ConflictingCourse: 'BVO 311C',
+        SectionNumber: '47280',
+    },
+};
