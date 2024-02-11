da9b1b08cdaf0d5a045843b7bbf8922aeb9dedbf
Razboy20 <razboy20@gmail.com>
2024-02-10 18:55:54 -0600

    tweak storybook rendering

--- src/stories/components/CourseStatus.stories.tsx
+++ src/stories/components/CourseStatus.stories.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
+
+import { Status } from '@shared/types/Course';
 import { Meta, StoryObj } from '@storybook/react';
-import CourseStatus from "@views/components/common/CourseStatus/CourseStatus"
-import { Status } from "@shared/types/Course"
+import CourseStatus from '@views/components/common/CourseStatus/CourseStatus';
 
 const meta = {
     title: 'Components/Common/CourseStatus',
@@ -9,18 +11,38 @@ const meta = {
         layout: 'centered',
     },
     tags: ['autodocs'],
+    args: {
+        status: Status.WAITLISTED,
+        size: 'small',
+    },
     argTypes: {
-        status: { control: 'object' },
-        size: { control: 'text' }
+        status: {
+            options: Object.values(Status),
+            mapping: Object.values(Status),
+            control: {
+                type: 'select',
+                labels: Object.keys(Status),
+            },
+        },
+        size: {
+            options: ['small', 'mini'],
+            control: {
+                type: 'radio',
+            },
+        },
     },
 } satisfies Meta<typeof CourseStatus>;
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-    args: {
-        status: Status.WAITLISTED,
-        size: 'small'
-    },
+export const Default: Story = {};
+
+export const Variants: Story = {
+    render: args => (
+        <div className='flex flex-col gap-4 items-center'>
+            <CourseStatus {...args} size='small' />
+            <CourseStatus {...args} size='mini' />
+        </div>
+    ),
 };
