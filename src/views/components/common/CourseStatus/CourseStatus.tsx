4e0464543aebe4ec399156078467ebfe52c34da8
Razboy20 <razboy20@gmail.com>
2024-02-10 18:55:45 -0600

    tweak to match figma designs

--- src/views/components/common/CourseStatus/CourseStatus.tsx
+++ src/views/components/common/CourseStatus/CourseStatus.tsx
@@ -1,20 +1,17 @@
-import React from 'react';
 import { Status } from '@shared/types/Course';
 import { StatusIcon } from '@shared/util/icons';
 import clsx from 'clsx';
+import React from 'react';
 import Text from '../Text/Text';
 
 type SizeType = 'small' | 'mini';
-
-
-  
 
 /**
  * Props for CourseStatus
  */
 export interface CourseStatusProps {
-    status: Status,
-    size: SizeType
+    status: Status;
+    size: SizeType;
 }
 
 /**
@@ -24,18 +21,16 @@ export interface CourseStatusProps {
  */
 export default function CourseStatus({ status, size }: CourseStatusProps): JSX.Element {
     const statusIconSizeClass = clsx({
-        'h-5 w-5': size === 'small', 
-        'h-4 w-4': size === 'mini',  
+        'h-5 w-5': size === 'small',
+        'h-4 w-4': size === 'mini',
     });
 
     return (
-        <div className="inline-flex items-center gap-1">
-            <StatusIcon status={status} className={statusIconSizeClass} />
-            <Text
-                variant={size}
-            >
-                {status}
-            </Text>                
+        <div className={`inline-flex items-center ${size === 'small' ? 'gap-2' : 'gap-1.5'}`}>
+            <div className='ml-1 flex items-center justify-center rounded bg-slate-700 p-1px text-white'>
+                <StatusIcon status={status} className={statusIconSizeClass} />
+            </div>
+            <Text variant={size}>{status}</Text>
         </div>
     );
 }
