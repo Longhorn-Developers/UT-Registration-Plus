118514e8b72710ae3f1582c29c28f3ff438957ca
knownotunknown <78577376+knownotunknown@users.noreply.github.com>
2024-02-13 13:38:02 -0600

    Storybook works

--- src/views/components/PopupMain.tsx
+++ src/views/components/PopupMain.tsx
@@ -1,21 +1,21 @@
 import { background } from '@shared/messages';
 import React from 'react';
-import useSchedules from '../hooks/useSchedules';
+//import useSchedules from '../hooks/useSchedules';
 import { Button } from './common/Button/Button';
 import ExtensionRoot from './common/ExtensionRoot/ExtensionRoot';
 
 export default function PopupMain() {
-    const [activeSchedule, schedules] = useSchedules();
+    //const [activeSchedule, schedules] = useSchedules();
 
     // TODO: Add a button to to switch the active schedule
 
     return (
         <ExtensionRoot>
             <Button
-                onClick={() => {
+                /*onClick={() => {
                     if (!activeSchedule) return;
                     background.clearCourses({ scheduleName: activeSchedule?.name });
-                }}
+                }}*/
             >
                 Clear Courses
             </Button>
