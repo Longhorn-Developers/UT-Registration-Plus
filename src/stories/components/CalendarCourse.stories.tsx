d390af3c79f0b68621e762b62a2fb0c5854f8873
knownotunknown <78577376+knownotunknown@users.noreply.github.com>
2024-02-14 13:24:55 -0600

    chore: update CalendarCourse.stories.tsx

--- src/stories/components/CalendarCourse.stories.tsx
+++ src/stories/components/CalendarCourse.stories.tsx
@@ -1,9 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { Course, Status } from 'src/shared/types/Course';
-import { CourseMeeting, DAY_MAP } from 'src/shared/types/CourseMeeting';
-import { CourseSchedule } from 'src/shared/types/CourseSchedule';
-import Instructor from 'src/shared/types/Instructor';
-import CalendarCourse from 'src/views/components/common/CalendarCourseBlock/CalendarCourseMeeting';
+import { Course, Status } from '@shared/types/Course';
+import { CourseMeeting, DAY_MAP } from '@shared/types/CourseMeeting';
+import { CourseSchedule } from '@shared/types/CourseSchedule';
+import Instructor from '@shared/types/Instructor';
+import CalendarCourse from '@views/components/common/CalendarCourseBlock/CalendarCourseMeeting';
 
 const meta = {
     title: 'Components/Common/CalendarCourseMeeting',
