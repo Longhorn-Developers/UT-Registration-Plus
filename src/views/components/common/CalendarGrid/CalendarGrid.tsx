e13b23e6f7ba492d7728150bb4f5d6ac39c91c29
Vinson Zheng <vinson@wireless-10-148-255-33.public.utexas.edu>
2024-02-17 11:45:27 -0600

    chore: ran eslint on Calendar component

--- src/views/components/common/CalendarGrid/CalendarGrid.tsx
+++ src/views/components/common/CalendarGrid/CalendarGrid.tsx
@@ -1,36 +1,35 @@
 import React from 'react';
+import { DAY_MAP } from 'src/shared/types/CourseMeeting';
 import styles from './CalendarGrid.module.scss';
 import CalendarCell from '../CalendarGridCell/CalendarGridCell';
-import { DAY_MAP } from 'src/shared/types/CourseMeeting';
 
 const daysOfWeek = Object.keys(DAY_MAP).filter(key => !['S', 'SU'].includes(key));
 const hoursOfDay = Array.from({ length: 14 }, (_, index) => index + 8);
 const grid = [];
 for (let i = 0; i < 13; i++) {
-  const row = [];
-  let hour = hoursOfDay[i];
-  row.push(<>
-    <div key={hour} className={styles.timeBlock}>
-      <div className={styles.timeLabelContainer}><p>{(hour % 12 === 0 ? 12 : hour % 12) + (hour < 12 ? ' AM' : ' PM')}</p></div>
-    </div>
-  </>
-  );
-  row.push(Array.from({ length: 5 }, (_, j) => <CalendarCell key={j} />));
-  grid.push(row);
+    const row = [];
+    let hour = hoursOfDay[i];
+    row.push(
+        <div key={hour} className={styles.timeBlock}>
+            <div className={styles.timeLabelContainer}>
+                <p>{(hour % 12 === 0 ? 12 : hour % 12) + (hour < 12 ? ' AM' : ' PM')}</p>
+            </div>
+        </div>
+    );
+    row.push(Array.from({ length: 5 }, (_, j) => <CalendarCell key={j} />));
+    grid.push(row);
 }
 
 /**
  * Grid of CalendarGridCell components forming the user's course schedule calendar view
- * @param props 
+ * @param props
  */
-const Calendar: React.FC = (props) => {
-  return (
+const Calendar: React.FC = props => (
     <div className={styles.calendar}>
-      <div className={styles.dayLabelContainer}>
-      </div>
-      {/* Displaying the rest of the calendar */}
-      <div className={styles.timeAndGrid}>
-        {/* <div className={styles.timeColumn}>
+        <div className={styles.dayLabelContainer} />
+        {/* Displaying the rest of the calendar */}
+        <div className={styles.timeAndGrid}>
+            {/* <div className={styles.timeColumn}>
           <div className={styles.timeBlock}></div>
           {hoursOfDay.map((hour) => (
             <div key={hour} className={styles.timeBlock}>
@@ -40,19 +39,18 @@ const Calendar: React.FC = (props) => {
             </div>
           ))}
         </div> */}
-        <div className={styles.calendarGrid}>
-          {/* Displaying day labels */}
-          <div className={styles.timeBlock}></div>
-          {daysOfWeek.map(day => (
-            <div key={day} className={styles.day}>
-              {day}
+            <div className={styles.calendarGrid}>
+                {/* Displaying day labels */}
+                <div className={styles.timeBlock} />
+                {daysOfWeek.map(day => (
+                    <div key={day} className={styles.day}>
+                        {day}
+                    </div>
+                ))}
+                {grid.map((row, rowIndex) => row)}
             </div>
-          ))}
-          {grid.map((row, rowIndex) => (row))}
         </div>
-      </div>
     </div>
-  )
-};
+);
 
 export default Calendar;
