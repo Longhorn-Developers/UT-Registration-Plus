42d24f636790a876810e3fa1ea7e24056298682b
DhruvArora-03 <dhruvarora@utexas.edu>
2024-02-17 13:02:06 -0600

    create empty settings component - waiting on design

---
+++ src/views/components/Settings.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {
+    className?: string;
+};
+
+/**
+ * Component to hold everything for the settings page
+ * @param props className
+ * @returns The content for the settings page
+ */
+export default function Settings({ className }: Props) {
+    return <div className={className}>this will be finished laterrrrrrr</div>;
+}
