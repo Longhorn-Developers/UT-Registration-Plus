5212784e8223956f40a876f848e9806d1b943ceb
Vinson Zheng <vinsonzheng499@gmail.com>
2024-02-17 15:44:12 -0600

    CalendarHeader alignment progress

--- src/views/components/common/CalendarHeader/CalenderHeader.tsx
+++ src/views/components/common/CalendarHeader/CalenderHeader.tsx
@@ -12,40 +12,29 @@ import ScheduleTotalHoursAndCourses from '../ScheduleTotalHoursAndCourses/Schedu
 import CourseStatus from '../CourseStatus/CourseStatus';
 
 const CalendarHeader = () => (
-    <div
-        style={{
-            display: 'flex',
-            minWidth: '672px',
-            minHeight: '79px',
-            padding: '15px 0px',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            alignContent: 'center',
-            rowGap: '10px',
-            alignSelf: 'stretch',
-            flexWrap: 'wrap',
-        }}
-    >
-        <Button variant='single' icon={MenuIcon} color='ut-gray' />
-
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-            <LogoIcon style={{ marginRight: '5px' }} />
-            <Text>Your Logo Text</Text>
+    <div className='min-h-79px min-w-672px flex flex-wrap items-center justify-between px-0 py-15'>
+        <div className='flex'>
+            <Button variant='single' icon={MenuIcon} color='ut-gray' />
+            <div className='flex items-center'>
+                <LogoIcon style={{ marginRight: '5px' }} />
+                <Text>UT Registration Plus</Text>
+            </div>
         </div>
 
-        <ScheduleTotalHoursAndCourses scheduleName='SCHEDULE' totalHours={22} totalCourses={8} />
-
-        <CourseStatus size='small' status={Status.WAITLISTED} />
-        <CourseStatus size='small' status={Status.CLOSED} />
-        <CourseStatus size='small' status={Status.CANCELLED} />
-
-        <div style={{ display: 'flex' }}>
+        <div className='flex flex-col'>
+            <ScheduleTotalHoursAndCourses scheduleName='SCHEDULE' totalHours={22} totalCourses={8} />
+            DATA UPDATED ON: 12:00 AM 02/01/2024
+        </div>
+        <div className='flex flex-row items-center'>
+            <CourseStatus size='small' status={Status.WAITLISTED} />
+            <CourseStatus size='small' status={Status.CLOSED} />
+            <CourseStatus size='small' status={Status.CANCELLED} />
+        </div>
+        <div className='flex'>
             <Button variant='outline' icon={UndoIcon} color='ut-black' />
             <Button variant='outline' icon={RedoIcon} color='ut-black' />
         </div>
-
         <Button variant='outline' icon={SettingsIcon} color='ut-black' />
-
         <Divider type='solid' />
     </div>
 );
