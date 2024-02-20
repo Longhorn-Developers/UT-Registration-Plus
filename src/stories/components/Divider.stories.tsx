a2a58cbfbaba499671b663b8c71364751dc85585
Samuel Gunter <sgunter@utexas.edu>
2024-02-20 16:41:10 -0600

    feat: updated divider component

---
+++ src/stories/components/Divider.stories.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import Divider from '@views/components/common/Divider/Divider';
+import { Button } from '../../views/components/common/Button/Button';
+import AddIcon from '~icons/material-symbols/add';
+import CalendarMonthIcon from '~icons/material-symbols/calendar-month';
+import DescriptionIcon from '~icons/material-symbols/description';
+import HappyFaceIcon from '~icons/material-symbols/mood';
+import ReviewsIcon from '~icons/material-symbols/reviews';
+
+const meta = {
+    title: 'Components/Common/Divider',
+    component: Divider,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    // argTypes: {},
+} satisfies Meta<typeof Divider>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Vertical: Story = {
+    args: {
+        size: '2.5rem',
+        variant: 'vertical',
+    },
+    render: props => <Divider {...props} />,
+};
+
+export const Horizontal: Story = {
+    args: {
+        size: '2.5rem',
+        variant: 'horizontal',
+    },
+    render: props => <Divider {...props} />,
+};
+
+export const IGotHorizontalIGotVerticalWhatYouWant: Story = {
+    args: {
+        size: '2.5rem',
+        variant: 'vertical',
+    },
+
+    render: props => (
+        <div
+            style={{
+                display: 'grid',
+                gridTemplate: 'repeat(3, 1fr) / repeat(7, 1fr)',
+                alignItems: 'center',
+                justifyItems: 'center',
+                gap: '15px',
+            }}
+        >
+            <Divider {...props} variant='horizontal' />
+            <Divider {...props} variant='vertical' />
+            <Divider {...props} variant='horizontal' />
+            <Divider {...props} variant='vertical' />
+            <Divider {...props} variant='horizontal' />
+            <Divider {...props} variant='vertical' />
+            <Divider {...props} variant='horizontal' />
+
+            <Divider {...props} variant='vertical' />
+            <Divider {...props} variant='horizontal' />
+            <Divider {...props} variant='vertical' />
+            <Divider {...props} variant='horizontal' />
+            <Divider {...props} variant='vertical' />
+            <Divider {...props} variant='horizontal' />
+            <Divider {...props} variant='vertical' />
+
+            <Divider {...props} variant='horizontal' />
+            <Divider {...props} variant='vertical' />
+            <Divider {...props} variant='horizontal' />
+            <Divider {...props} variant='vertical' />
+            <Divider {...props} variant='horizontal' />
+            <Divider {...props} variant='vertical' />
+            <Divider {...props} variant='horizontal' />
+        </div>
+    ),
+};
+
+export const CourseCatalogActionButtons: Story = {
+    args: {
+        size: '1.75rem',
+        variant: 'vertical',
+    },
+    render: props => (
+        <div style={{ display: 'flex', gap: '15px', alignItems: 'center' }}>
+            <Button variant='filled' color='ut-burntorange' icon={CalendarMonthIcon} />
+            <Divider {...props} />
+            <Button variant='outline' color='ut-blue' icon={ReviewsIcon}>
+                RateMyProf
+            </Button>
+            <Button variant='outline' color='ut-teal' icon={HappyFaceIcon}>
+                CES
+            </Button>
+            <Button variant='outline' color='ut-orange' icon={DescriptionIcon}>
+                Past Syllabi
+            </Button>
+            <Button variant='filled' color='ut-green' icon={AddIcon}>
+                Add Course
+            </Button>
+        </div>
+    ),
+};
