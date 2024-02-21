d5a04c745f328aae5c580643def83038bca6c782
doprz <52579214+doprz@users.noreply.github.com>
2024-02-21 15:54:21 -0600

    feat: Best Practices (#102)
    * feat: best practices
    
    * feat: add tests workflow
    
    * feat: add best-practices workflow
    
    * fix: wrong indentation in workflow

---
+++ src/shared/util/tests/time.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from 'vitest';
+
+import { sleep } from '../time';
+
+describe('sleep', () => {
+    it('should resolve after the specified number of milliseconds', async () => {
+        const start = Date.now();
+        const milliseconds = 1000;
+        await sleep(milliseconds);
+        const end = Date.now();
+        const elapsed = end - start;
+        expect(elapsed).toBeGreaterThanOrEqual(milliseconds);
+    });
+});
