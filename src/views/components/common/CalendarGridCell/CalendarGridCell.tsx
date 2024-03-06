5a8f6a8f1f588165d9b54d8600329af6034f7e41
doprz <52579214+doprz@users.noreply.github.com>
2024-03-06 13:03:05 -0600

    chore: add docs

--- src/views/components/common/CalendarGridCell/CalendarGridCell.tsx
+++ src/views/components/common/CalendarGridCell/CalendarGridCell.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styles from './CalendarGridCell.module.scss';
 
+/**
+ * Component representing each 1 hour time block of a calendar
+ * @param props
+ */
 const CalendarCell: React.FC = props => {
     return (
         <div className={styles.calendarCell}>
