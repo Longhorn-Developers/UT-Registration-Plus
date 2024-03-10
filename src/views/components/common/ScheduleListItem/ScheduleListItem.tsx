261d2f2e84a09480111c5e8874aeeab2610853ec
Razboy20 <razboy20@gmail.com>
2024-03-09 23:16:56 -0600

    refactor: match dropdown to figma & fix issues (#142)

--- src/views/components/common/ScheduleListItem/ScheduleListItem.tsx
+++ src/views/components/common/ScheduleListItem/ScheduleListItem.tsx
@@ -1,6 +1,7 @@
 import Text from '@views/components/common/Text/Text';
+import useSchedules from '@views/hooks/useSchedules';
 import clsx from 'clsx';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import DragIndicatorIcon from '~icons/material-symbols/drag-indicator';
 
@@ -11,34 +12,35 @@ export type Props = {
     style?: React.CSSProperties;
     active?: boolean;
     name: string;
-    dragHandleProps?: any;
-    onClick?: (index) => void;
+    dragHandleProps?: Omit<React.HTMLAttributes<HTMLDivElement>, 'className'>;
+    onClick?: React.DOMAttributes<HTMLDivElement>['onClick'];
 };
 
 /**
  * This is a reusable dropdown component that can be used to toggle the visiblity of information
  */
-export default function ScheduleListItem({ style, active, name, dragHandleProps, onClick }: Props): JSX.Element {
+export default function ScheduleListItem({ style, name, dragHandleProps, onClick }: Props): JSX.Element {
+    const [activeSchedule] = useSchedules();
+
+    const isActive = useMemo(() => activeSchedule?.name === name, [activeSchedule, name]);
+
     return (
-        <div style={{ ...style }} className='items-center'>
+        <div style={{ ...style }} className='items-center rounded bg-white'>
             <li className='w-100% flex cursor-pointer items-center self-stretch justify-left text-ut-burntorange'>
-                <div className='group flex justify-center'>
+                <div className='flex justify-center'>
                     <div
                         className='flex cursor-move items-center self-stretch rounded rounded-r-0'
                         {...dragHandleProps}
                     >
                         <DragIndicatorIcon className='h-6 w-6 cursor-move text-zinc-300 btn-transition -ml-1.5 hover:text-zinc-400' />
                     </div>
-                    <div className='inline-flex items-center justify-center gap-1.5'>
-                        <div
-                            className='h-5.5 w-5.5 flex items-center justify-center border-2px border-current rounded-full btn-transition group-active:scale-95'
-                            onClick={onClick}
-                        >
+                    <div className='group inline-flex items-center justify-center gap-1.5' onClick={onClick}>
+                        <div className='h-5.5 w-5.5 flex items-center justify-center border-2px border-current rounded-full btn-transition group-active:scale-95'>
                             <div
                                 className={clsx(
-                                    'bg-current h-3 w-3 rounded-full transition tansform scale-100 ease-out-expo duration-250',
+                                    'bg-current h-2.9 w-2.9 rounded-full transition tansform scale-100 ease-out-expo duration-250',
                                     {
-                                        'scale-0! opacity-0 ease-in-out! duration-200!': !active,
+                                        'scale-0! opacity-0 ease-in-out! duration-200!': !isActive,
                                     }
                                 )}
                             />
