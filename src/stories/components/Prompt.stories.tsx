7e95792959a476e835ee302f51e7e4813753819b
Abhinav Chadaga <abhinav.chadaga@utexas.edu>
2024-03-11 18:38:49 -0500

    feat: add some stuff

--- src/stories/components/Prompt.stories.tsx
+++ src/stories/components/Prompt.stories.tsx
@@ -1,9 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '@views/components/common/Button/Button';
+import type { PromptDialogProps } from '@views/components/common/Prompt/Prompt';
 import PromptDialog from '@views/components/common/Prompt/Prompt';
+import Text from '@views/components/common/Text/Text';
 import React from 'react';
 
-const meta = {
+const meta: Meta<typeof PromptDialog> = {
     title: 'Components/Common/Prompt',
     component: PromptDialog,
     parameters: {
@@ -12,26 +14,43 @@ const meta = {
     tags: ['autodocs'],
     argTypes: {
         isOpen: { control: 'boolean' },
-        title: { control: 'text' },
-        content: { control: 'text' },
+        title: { control: 'object' },
+        content: { control: 'object' },
         children: { control: 'object' },
     },
-} satisfies Meta<typeof PromptDialog>;
+};
 export default meta;
 
-type Story = StoryObj<typeof meta>;
+// Define a new component for use in the story to comply with React's rules of hooks
+const PromptDialogWithButton = (args: PromptDialogProps) => {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const handleOpen = () => setIsOpen(true);
+    const handleClose = () => setIsOpen(false);
 
-export const Default: Story = {
+    return (
+        <div className='h-screen w-screen flex items-center justify-center'>
+            <Button variant='filled' color='ut-burntorange' onClick={handleOpen}>
+                Open Prompt
+            </Button>
+            <PromptDialog {...args} isOpen={isOpen} onClose={handleClose} />
+        </div>
+    );
+};
+
+export const Default: StoryObj<PromptDialogProps> = {
+    render: args => <PromptDialogWithButton {...args} />, // Use the new component here
     args: {
-        isOpen: true,
-        onClose: () => {},
-        title: 'Are you sure?',
-        content: 'This will delete all of your courses and schedules. Are you sure you want to continue?',
+        title: <Text variant='h2'>Are you sure?</Text>,
+        content: (
+            <Text variant='p'>
+                Deleting Main Schedule is permanent and will remove all added courses and schedules.
+            </Text>
+        ),
         children: [
-            <Button key='yes' variant='filled' color='ut-burntorange' onClick={() => {}}>
+            <Button key='yes' variant='single' color='ut-burntorange' onClick={() => {}}>
                 Yes
             </Button>,
-            <Button key='no' variant='outline' color='ut-black' onClick={() => {}}>
+            <Button key='no' variant='single' color='ut-black' onClick={() => {}}>
                 No
             </Button>,
         ],
--- src/views/components/common/Prompt/Prompt.tsx
+++ src/views/components/common/Prompt/Prompt.tsx
@@ -1,30 +1,30 @@
-import { Dialog, Transition } from '@headlessui/react';
+import { Dialog } from '@headlessui/react';
 import React from 'react';
 
 import type { Button } from '../Button/Button';
+import type Text from '../Text/Text';
 
 export interface PromptDialogProps {
     isOpen: boolean;
     onClose: () => void;
-    title: string;
-    content: string;
+    title: React.ReactElement<typeof Text>;
+    content: React.ReactElement<typeof Text>;
     children?: React.ReactElement<typeof Button>[];
 }
 
-const PromptDialog: React.FC<PromptDialogProps> = ({
-    isOpen,
-    onClose,
-    title,
-    content,
-    children,
-}: PromptDialogProps) => (
-    <Dialog open={isOpen} onClose={onClose}>
-        <Dialog.Panel>
-            <Dialog.Title>{title}</Dialog.Title>
-            <Dialog.Description>{content}</Dialog.Description>
-            <div className='flex items-center justify-end gap-2'>{children}</div>
-        </Dialog.Panel>
-    </Dialog>
-);
+function PromptDialog({ isOpen, onClose, title, content, children }: PromptDialogProps) {
+    return (
+        <Dialog open={isOpen} onClose={onClose} className='relative z-50'>
+            <div className='fixed inset-0 bg-black bg-opacity-50' aria-hidden='true' />
+            <div className='fixed inset-0 w-screen flex items-center justify-center'>
+                <Dialog.Panel className='h-[180] w-[409px] flex flex-col rounded bg-white p-6'>
+                    <Dialog.Title className='mb-[10px]'>{title}</Dialog.Title>
+                    <Dialog.Description className='mb-[13px]'>{content}</Dialog.Description>
+                    <div className='flex items-center justify-end gap-2'>{children}</div>
+                </Dialog.Panel>
+            </div>
+        </Dialog>
+    );
+}
 
 export default PromptDialog;
