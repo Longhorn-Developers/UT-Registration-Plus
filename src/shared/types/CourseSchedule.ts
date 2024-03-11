68242bf6175e7323161496800a58bcc25aff6c07
Lukas Zenick <lukas@utexas.edu>
2024-03-11 15:50:04 -0500

    fix: injection not working from som/elie commit

--- src/shared/types/CourseSchedule.ts
+++ src/shared/types/CourseSchedule.ts
@@ -7,7 +7,7 @@ import { CourseMeeting, DAY_MAP } from './CourseMeeting';
  * This represents the schedule for a course, which includes all the meeting times for the course, as well as helper functions for parsing, serializing, and deserializing the schedule
  */
 export class CourseSchedule {
-    meetings: CourseMeeting[];
+    meetings: CourseMeeting[] = [];
 
     constructor(courseSchedule?: Serialized<CourseSchedule>) {
         if (!courseSchedule) {
