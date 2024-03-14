ed9e3eb115e53f86e074c2551c0875591d977942
doprz <52579214+doprz@users.noreply.github.com>
2024-03-13 23:57:32 -0500

    feat: add new db powered by UT_Grade_Parser

--- src/shared/types/Distribution.ts
+++ src/shared/types/Distribution.ts
@@ -1,40 +1,38 @@
-import { PointOptionsObject } from 'highcharts';
-
-import { Semester } from './Course';
 /**
  * Each of the possible letter grades that can be given in a course
  */
-export type LetterGrade = 'A' | 'A-' | 'B' | 'B+' | 'B-' | 'C' | 'C+' | 'C-' | 'D' | 'D+' | 'D-' | 'F';
+export type LetterGrade = 'A' | 'A-' | 'B' | 'B+' | 'B-' | 'C' | 'C+' | 'C-' | 'D' | 'D+' | 'D-' | 'F' | 'Other';
 
 /**
  * A distribution of grades for a course,
  * with the keys being the letter grades and the values being the number of students that got that grade
  */
-export type Distribution = {
-    [key in LetterGrade]: number;
-};
+// export type Distribution = {
+//     [key in LetterGrade]: number;
+// };
 
 /**
  * This is a object-ified version of a row in the SQL table that is used to store the distribution data.
  */
 export type CourseSQLRow = {
-    sem?: string;
-    prof?: string;
-    dept?: string;
-    course_nbr?: string;
-    course_name?: string;
-    a1?: number;
-    a2?: number;
-    a3?: number;
-    b1?: number;
-    b2?: number;
-    b3?: number;
-    c1?: number;
-    c2?: number;
-    c3?: number;
-    d1?: number;
-    d2?: number;
-    d3?: number;
-    f?: number;
-    semesters?: string;
+    semester: string;
+    section: number;
+    department: string;
+    department_code: string;
+    courseNumber: string;
+    courseTitle: string;
+    courseFullTitle: string;
+    a: number;
+    aMinus: number;
+    bPlus: number;
+    b: number;
+    bMinus: number;
+    cPlus: number;
+    c: number;
+    cMinus: number;
+    dPlus: number;
+    d: number;
+    dMinus: number;
+    f: number;
+    other: number;
 };
