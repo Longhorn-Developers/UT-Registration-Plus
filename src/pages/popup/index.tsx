01e94bf016c43cb42a9b777b8a44fa461c2790c8
doprz <52579214+doprz@users.noreply.github.com>
2024-03-15 12:09:11 -0500

    fix: strict TS errors

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -4,4 +4,9 @@ import PopupMain from '@views/components/PopupMain';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(<PopupMain />);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    createRoot(rootElement).render(<PopupMain />);
+} else {
+    throw new Error('Could not find root element');
+}
