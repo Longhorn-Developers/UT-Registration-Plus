53d74d25cab9a335c53d157573b70e847432e3b9
doprz <52579214+doprz@users.noreply.github.com>
2024-03-15 12:08:55 -0500

    fix: strict TS errors

--- src/pages/options/index.tsx
+++ src/pages/options/index.tsx
@@ -3,4 +3,9 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-createRoot(document.getElementById('root')).render(<App />);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    createRoot(rootElement).render(<App />);
+} else {
+    throw new Error('Could not find root element');
+}
