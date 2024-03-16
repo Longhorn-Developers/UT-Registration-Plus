2b59df74873ba5080c29b54aee8fbfd8a0d20df4
doprz <52579214+doprz@users.noreply.github.com>
2024-03-15 20:54:12 -0500

    fix: strict TS errors

--- src/views/components/calendar/CalendarCourseBlock/CalendarCourseMeeting.tsx
+++ src/views/components/calendar/CalendarCourseBlock/CalendarCourseMeeting.tsx
@@ -29,14 +29,20 @@ export default function CalendarCourseMeeting({
     meetingIdx,
     color,
     rightIcon,
-}: CalendarCourseMeetingProps): JSX.Element {
-    let meeting: CourseMeeting | null = meetingIdx !== undefined ? course.schedule.meetings[meetingIdx] : null;
+}: CalendarCourseMeetingProps): JSX.Element | null {
+    let meeting: CourseMeeting | null | undefined =
+        meetingIdx !== undefined ? course.schedule.meetings[meetingIdx] : null;
+
+    if (!meeting) {
+        return null;
+    }
+
     return (
         <div className={styles.component}>
             <div className={styles.content}>
                 <div className={styles['course-detail']}>
                     <div className={styles.course}>
-                        {course.department} {course.number} - {course.instructors[0].lastName}
+                        {course.department} {course.number} - {course.instructors[0]?.lastName}
                     </div>
                     <div className={styles['time-and-location']}>
                         {`${meeting.getTimeString({ separator: '-', capitalize: true })}${
