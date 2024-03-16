ed4fbe56519f79decd907d9cb775256575f9faf8
Razboy20 <razboy20@gmail.com>
2024-03-15 23:49:26 -0500

    refactor: ccpopup (#172)
    * refactor: dialog animation improvements
    
    * refactor: update ccpopup to match designs

--- src/views/components/common/Chip/Chip.tsx
+++ src/views/components/common/Chip/Chip.tsx
@@ -27,7 +27,7 @@ export function Chip({ label }: React.PropsWithChildren<Props>): JSX.Element {
         <Text
             as='div'
             variant='h4'
-            className='min-w-5 inline-flex items-center justify-center gap-2.5 rounded-lg px-1 py-0.5'
+            className='min-w-5 inline-flex items-center justify-center gap-2.5 rounded-lg px-1.5 py-0.5'
             style={{
                 backgroundColor: '#FFD600',
             }}
