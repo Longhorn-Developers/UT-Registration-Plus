d04818ccd8de2f136751acd890f8741b5bf7475d
Razboy20 <razboy20@gmail.com>
2024-03-15 22:06:36 -0500

    refactor: update text and link components to be polymorphic (#171)

--- src/views/components/common/Text/Text.tsx
+++ src/views/components/common/Text/Text.tsx
@@ -1,34 +1,49 @@
+import type { PropsOf, ReactTag } from '@headlessui/react/dist/types';
 import clsx from 'clsx';
-import type { PropsWithChildren } from 'react';
+import type { ElementType, ReactNode } from 'react';
 import React from 'react';
 
 import styles from './Text.module.scss';
 
-/**
- * Props for the Text component.
- */
-export type TextProps = {
-    variant?: Variant;
-} & (
-    | (React.HTMLAttributes<HTMLSpanElement> & { as?: 'span' })
-    | (React.HTMLAttributes<HTMLDivElement> & { as: 'div' })
-);
+type PropsWeControl = 'as' | 'children';
+type CleanProps<TTag extends ReactTag, TOmitableProps extends PropertyKey = never> = Omit<
+    PropsOf<TTag>,
+    TOmitableProps | PropsWeControl
+>;
+type OurProps<TTag extends ReactTag> = {
+    as?: TTag;
+    children?: ReactNode;
+};
+
+type AsProps<TTag extends ReactTag, TOverrides = {}> = CleanProps<TTag, keyof TOverrides> & OurProps<TTag> & TOverrides;
 
 const variants = ['mini', 'small', 'p', 'h4', 'h3-course', 'h3', 'h2-course', 'h2', 'h1-course', 'h1'] as const;
 
 type Variant = (typeof variants)[number];
 
 /**
+ * Props for the Text component.
+ */
+export type TextProps<TTag extends ElementType = 'span'> = PropsOf<TTag>['className'] extends string
+    ? AsProps<
+          TTag,
+          {
+              variant?: Variant;
+          }
+      >
+    : never;
+
+/**
  * A reusable Text component with props that build on top of the design system for the extension
  */
-export default function Text({ variant, as, className, ...props }: PropsWithChildren<TextProps>): JSX.Element {
-    const mergedClassName = clsx(styles.text, styles[variant], className);
+export default function Text<TTag extends ElementType = 'span'>({
+    as,
+    className,
+    variant,
+    ...rest
+}: TextProps<TTag>): JSX.Element {
+    const Comp = as || 'span';
+    const mergedClassName = clsx(styles.text, styles[variant || 'p'], className);
 
-    if (as === 'div') return <div className={mergedClassName} {...props} />;
-
-    return (
-        <span className={mergedClassName} {...props}>
-            {props.children}
-        </span>
-    );
+    return <Comp className={mergedClassName} {...rest} />;
 }
