6f84bb4bf6e77c2b33379be757f7753b671d7b5c
doprz <52579214+doprz@users.noreply.github.com>
2024-03-15 20:17:28 -0500

    fix: strict TS errors

--- src/shared/util/storybook.ts
+++ src/shared/util/storybook.ts
@@ -3,6 +3,6 @@ import { theme } from 'unocss/preset-mini';
 
 export const tailwindColorways = Object.keys(theme.colors)
     // check that the color is a colorway (is an object)
-    .filter(color => typeof theme.colors[color] === 'object')
+    .filter((color: string) => typeof theme.colors[color as keyof typeof theme.colors] === 'object')
     .slice(0, 17)
     .map(color => getCourseColors(color as keyof typeof theme.colors));
