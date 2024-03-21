096f02e2cd664d58984743f71cff92e2d54c66c8
doprz <52579214+doprz@users.noreply.github.com>
2024-03-21 16:25:37 -0500

    feat: use downloadBlob util (#186)
    * feat: use downloadBlob util
    
    * chore: lint
    
    * fix: revert saveCalAsPng
    
    * feat: refactor downloadBlob
    
    * chore: remove comments
    
    * chore: lint and remove extra async
    
    * refactor: cleanup
    
    ---------
    
    Co-authored-by: Razboy20 <razboy20@gmail.com>

--- src/shared/types/MIMEType.ts
+++ src/shared/types/MIMEType.ts
@@ -8,7 +8,10 @@ const MIMEType = {
     IMAGE: 'image/*',
     AUDIO: 'audio/*',
     VIDEO: 'video/*',
+    CALENDAR: 'text/calendar',
     ANY: '*/*',
 } as const satisfies Record<string, string>;
+
+export type MIMETypeKey = keyof typeof MIMEType;
 
 export default MIMEType;
