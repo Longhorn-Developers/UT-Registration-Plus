08b854beaccf7b73c806dd2895e89c178f86a778
Samuel Gunter <sgunter@utexas.edu>
2024-03-20 21:10:51 -0500

    feat: wrap Prompt Dialog in an ExtensionRoot instead of just font-sans

--- src/views/components/common/Prompt/Prompt.tsx
+++ src/views/components/common/Prompt/Prompt.tsx
@@ -3,6 +3,7 @@ import type { ReactElement } from 'react';
 import React from 'react';
 
 import type { Button } from '../Button/Button';
+import ExtensionRoot from '../ExtensionRoot/ExtensionRoot';
 import type Text from '../Text/Text';
 
 /**
@@ -27,36 +28,38 @@ export interface PromptDialogProps {
 function PromptDialog({ isOpen, onClose, title, content, children }: PromptDialogProps) {
     return (
         <Transition appear show={isOpen} as={React.Fragment}>
-            <Dialog as='div' onClose={onClose} className='relative z-50 font-sans'>
-                <Transition.Child
-                    as={React.Fragment}
-                    enter='ease-out duration-200'
-                    enterFrom='opacity-0'
-                    enterTo='opacity-100'
-                    leave='ease-in duration-200'
-                    leaveFrom='opacity-100'
-                    leaveTo='opacity-0'
-                >
-                    <div className='fixed inset-0 bg-black bg-opacity-50' aria-hidden='true' />
-                </Transition.Child>
+            <Dialog as='div' onClose={onClose} className='relative z-50'>
+                <ExtensionRoot>
+                    <Transition.Child
+                        as={React.Fragment}
+                        enter='ease-out duration-200'
+                        enterFrom='opacity-0'
+                        enterTo='opacity-100'
+                        leave='ease-in duration-200'
+                        leaveFrom='opacity-100'
+                        leaveTo='opacity-0'
+                    >
+                        <div className='fixed inset-0 bg-black bg-opacity-50' aria-hidden='true' />
+                    </Transition.Child>
 
-                <Transition.Child
-                    as={React.Fragment}
-                    enter='ease-out duration-200'
-                    enterFrom='opacity-0 scale-95'
-                    enterTo='opacity-100 scale-100'
-                    leave='ease-in duration-200'
-                    leaveFrom='opacity-100 scale-100'
-                    leaveTo='opacity-0 scale-95'
-                >
-                    <div className='fixed inset-0 w-screen flex items-center justify-center'>
-                        <Dialog.Panel className='h-[200] w-[431px] flex flex-col rounded bg-white p-6'>
-                            <Dialog.Title className='mb-[10px]'>{title}</Dialog.Title>
-                            <Dialog.Description className='mb-[13px]'>{content}</Dialog.Description>
-                            <div className='flex items-center justify-end gap-2'>{children}</div>
-                        </Dialog.Panel>
-                    </div>
-                </Transition.Child>
+                    <Transition.Child
+                        as={React.Fragment}
+                        enter='ease-out duration-200'
+                        enterFrom='opacity-0 scale-95'
+                        enterTo='opacity-100 scale-100'
+                        leave='ease-in duration-200'
+                        leaveFrom='opacity-100 scale-100'
+                        leaveTo='opacity-0 scale-95'
+                    >
+                        <div className='fixed inset-0 w-screen flex items-center justify-center'>
+                            <Dialog.Panel className='h-[200] w-[431px] flex flex-col rounded bg-white p-6'>
+                                <Dialog.Title className='mb-[10px]'>{title}</Dialog.Title>
+                                <Dialog.Description className='mb-[13px]'>{content}</Dialog.Description>
+                                <div className='flex items-center justify-end gap-2'>{children}</div>
+                            </Dialog.Panel>
+                        </div>
+                    </Transition.Child>
+                </ExtensionRoot>
             </Dialog>
         </Transition>
     );
