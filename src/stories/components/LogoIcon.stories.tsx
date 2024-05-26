3eeebaffd56f4a57b7f46b0218acb2dcf58f528e
Derek Chen <derex1987@gmail.com>
2024-05-26 16:25:13 -0500

    chore: styled up logo stories

--- src/stories/components/LogoIcon.stories.tsx
+++ src/stories/components/LogoIcon.stories.tsx
@@ -11,8 +11,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Small: Story = {
-};
+export const Small: Story = {};
 
 export const Large: Story = {
     render: args => <LargeLogo {...args} />,
