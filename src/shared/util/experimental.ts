9ad1fb90fda3e4825623e445d06adcd5f9c83866
Derek Chen <derex1987@gmail.com>
2024-09-29 01:33:28 -0500

    feat: fine it'll be a constant

--- src/shared/util/experimental.ts
+++ src/shared/util/experimental.ts
@@ -1,12 +1,8 @@
-export let toggleIcons: boolean = false;
-export function enableIcons() {
-    toggleIcons = true;
-}
+export const toggleIcons: boolean = false;
+// export function enableIcons() {
+//     toggleIcons = true;
+// }
 
-export function disableIcons() {
-    toggleIcons = false;
-}
-
-export function getIcons(): boolean {
-    return toggleIcons;
-}
+// export function disableIcons() {
+//     toggleIcons = false;
+// }
