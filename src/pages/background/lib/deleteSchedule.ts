53e898a2acaa2b8f73c83582232f6ac6656ffa5b
Derek Chen <derex1987@gmail.com>
2024-10-04 05:38:51 -0500

    feat: undo deleteSchedule changes

--- src/pages/background/lib/deleteSchedule.ts
+++ src/pages/background/lib/deleteSchedule.ts
@@ -1,6 +1,4 @@
 import { UserScheduleStore } from '@shared/storage/UserScheduleStore';
-import { usePrompt } from 'src/views/components/common/DialogProvider/DialogProvider';
-import PromptDialog from 'src/views/components/common/Prompt';
 
 /**
  * Deletes a schedule with the specified name.
@@ -9,8 +7,6 @@ import PromptDialog from 'src/views/components/common/Prompt';
  * @returns A promise that resolves to a string if there is an error, or undefined if the schedule is deleted successfully.
  */
 export default async function deleteSchedule(scheduleId: string): Promise<string | undefined> {
-    const showDialog = usePrompt();
-
     const [schedules, activeIndex] = await Promise.all([
         UserScheduleStore.get('schedules'),
         UserScheduleStore.get('activeIndex'),
@@ -23,18 +19,6 @@ export default async function deleteSchedule(scheduleId: string): Promise<string
     if (scheduleIndex === activeIndex) {
         throw new Error('You cannot delete your active schedule! Please switch to another schedule before deleting.');
     }
-
-    showDialog({
-        title: "Confirm Delete",
-        description: "Temp",
-        // eslint-disable-next-line react/no-unstable-nested-components
-        buttons: close => (
-            <Button variant='filled' color='ut-black' onClick={close}>
-                I understand
-            </Button>
-        ),
-        onClose: () => setError(undefined),
-    });
 
     schedules.splice(scheduleIndex, 1);
     await UserScheduleStore.set('schedules', schedules);
