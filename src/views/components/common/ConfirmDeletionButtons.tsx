01d5d7fca2db0559095d1d1052068c6db77651d2
doprz <52579214+doprz@users.noreply.github.com>
2024-10-04 11:09:18 -0500

    chore: update ConfirmDeleteButtons to function component syntax

--- src/views/components/common/ConfirmDeletionButtons.tsx
+++ src/views/components/common/ConfirmDeletionButtons.tsx
@@ -6,13 +6,15 @@ interface DialogButtonsProps {
     onDelete: () => void;
 }
 
-export const ConfirmDeleteButtons: React.FC<DialogButtonsProps> = ({ onClose, onDelete }) => (
-    <>
-        <Button variant='filled' color='ut-black' onClick={onClose}>
-            Cancel
-        </Button>
-        <Button variant='filled' color='ut-red' onClick={onDelete}>
-            Delete
-        </Button>
-    </>
-);
+export function ConfirmDeleteButtons({ onClose, onDelete }: DialogButtonsProps): JSX.Element {
+    return (
+        <>
+            <Button variant="filled" color="ut-black" onClick={onClose}>
+                Cancel
+            </Button>
+            <Button variant="filled" color="ut-red" onClick={onDelete}>
+                Delete
+            </Button>
+        </>
+    );
+}
