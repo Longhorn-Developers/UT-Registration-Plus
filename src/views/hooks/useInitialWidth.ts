1841ba3133f39d85c222832f48c8fd00aaab4742
Ethan Lanting <ethanlanting@gmail.com>
2024-10-07 23:43:30 -0500

    refactor: useInitialWidth jsdocs

--- src/views/hooks/useInitialWidth.ts
+++ src/views/hooks/useInitialWidth.ts
@@ -2,11 +2,7 @@ import { useLayoutEffect, useRef, useState } from 'react';
 
 /**
  * Custom hook to get the initial width of an HTML element.
- *
- * @template T - The type of the HTML element.
- * @returns {Object} An object containing the width of the element and a ref to the element.
- * @returns {number} width - The initial width of the element.
- * @returns {React.RefObject<T>} elementRef - A ref to the HTML element.
+ * @returns An object containing the width of the element and a ref to the element.
  */
 function useInitialWidth<T extends HTMLElement>() {
     const [width, setWidth] = useState<number>(0);
