14d486b2c87047db4c150fbe18d4d4081991337e
doprz <52579214+doprz@users.noreply.github.com>
2024-10-12 15:15:12 -0500

    chore: small ui and arg updates

--- src/views/components/common/UpdateText.tsx
+++ src/views/components/common/UpdateText.tsx
@@ -12,17 +12,17 @@ export default function UpdateText({ courses }: UpdateTextProps): JSX.Element {
     return (
         <div className='max-w-64 flex flex-col justify-center gap-2'>
             <div className='flex flex-col gap-0 text-center'>
-                <Text variant='p' className='text-ut-burntorange'>
+                <Text variant='h4' className='text-ut-burntorange'>
                     This extension has updated!
                 </Text>
                 <Text variant='p' className='text-ut-black'>
                     You may have already began planning your Spring 2025 schedule. Here are the Unique Numbers you had
-                    from the old version: (Please open each link and re- add course to your new schedule)
+                    from the old version: (Please open each link and re-add course to your new schedule)
                 </Text>
             </div>
             <div className='flex flex-col gap-1 text-center'>
-                {courses.map((course, index) => (
-                    <Text key={course} variant='small' className='text-ut-orange underline'>
+                {courses.map((course) => (
+                    <Text key={course} variant='p' className='text-ut-orange underline'>
                         {course}
                     </Text>
                 ))}
