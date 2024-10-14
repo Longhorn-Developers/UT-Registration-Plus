e45fe9bbc441ecd2a87fa65770de308de54e1a0d
Joseph Wilson <josephcwilson47@gmail.com>
2024-10-14 18:34:49 -0500

    feat: replace plus with minus for UTRP logo

--- src/views/components/common/LogoIcon.tsx
+++ src/views/components/common/LogoIcon.tsx
@@ -12,7 +12,6 @@ export function LogoIcon(props: SVGProps<SVGSVGElement>): JSX.Element {
         <svg width='40' height='40' viewBox='0 0 40 40' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
             <circle cx='20' cy='20' r='20' fill='#BF5700' />
             <circle cx='20' cy='20' r='15.5' stroke='white' strokeWidth='3' />
-            <rect x='18' y='10' width='4' height='19.5489' fill='white' />
             <rect x='10' y='22' width='4' height='19.5489' transform='rotate(-90 10 22)' fill='white' />
         </svg>
     );
