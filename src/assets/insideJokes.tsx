2e7a6ea161f310a044d7201269cf50147d6ce684
Christian Dipert <82560157+christiandipert@users.noreply.github.com>
2024-10-14 19:12:21 -0500

    Arch Manning Add

--- src/assets/insideJokes.tsx
+++ src/assets/insideJokes.tsx
@@ -115,6 +115,7 @@ const splashText: string[] = [
     `It's ${new Date().toLocaleString('en-US', { month: 'long', day: 'numeric' })} and OU still sucks`,
     'As seen on TV!	',
     "Should you major in Compsci? well, here's a better question. do you wanna have a bad time?",
+    "Archibald C. Manning 🙏🙏🙏🙏"
 ];
 
 export default splashText;
