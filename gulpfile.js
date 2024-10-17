e0821585927de92aa773e816af4c23b47087d298
Razboy20 <razboy20@gmail.com>
2024-10-16 22:18:18 -0500

    build: sentry instrumentation (#316)

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 // (Thanks go to https://github.com/pnd280/complexity/blob/alpha/gulpfile.js)
 
+import cp from 'child_process';
 import fs from 'fs';
 import gulp from 'gulp';
 import gulpZip from 'gulp-zip';
 import { createRequire } from 'module';
 
-function zip() {
+// Make sure sentry is configured https://docs.sentry.io/platforms/javascript/sourcemaps/uploading/typescript/#2-configure-sentry-cli
+function instrumentWithSentry() {
+    return cp.exec('sentry-cli sourcemaps inject dist/ && sentry-cli sourcemaps upload dist/');
+}
+
+function zipDist() {
     const require = createRequire(import.meta.url);
     const manifest = require('./package.json');
     const zipFileName = `${manifest.name.replaceAll(' ', '-')}-${manifest.version}.zip`;
@@ -17,6 +23,8 @@ function zip() {
         .pipe(gulpZip(zipFileName))
         .pipe(gulp.dest('package'));
 }
+
+const zip = gulp.series(instrumentWithSentry, zipDist);
 
 // Temp fix for CSP on Chrome 130
 // Manually remove them because there is no option to disable use_dynamic_url on @crxjs/vite-plugin
