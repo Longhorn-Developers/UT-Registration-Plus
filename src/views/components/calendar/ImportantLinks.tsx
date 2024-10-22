cef99c2d72d3a2800f8a918d01cb116f8795d0c8
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2024-10-21 19:13:11 -0500

    feat: update useful links (#367)
    added and rearranged links on calendar sidebar

--- src/views/components/calendar/ImportantLinks.tsx
+++ src/views/components/calendar/ImportantLinks.tsx
@@ -19,28 +19,28 @@ const links: LinkItem[] = [
         url: 'https://utdirect.utexas.edu/apps/registrar/course_schedule/20252/',
     },
     {
-        text: "Fall '24 Course Schedule",
-        url: 'https://utdirect.utexas.edu/apps/registrar/course_schedule/20249/',
+        text: 'Course Schedule Archives',
+        url: 'https://registrar.utexas.edu/schedules/archive',
     },
     // {
     //     text: "Summer '24 Course Schedule",
     //     url: 'https://utdirect.utexas.edu/apps/registrar/course_schedule/20246/',
     // },
     {
+        text: "'24-'25 Academic Calendar",
+        url: 'https://registrar.utexas.edu/calendars/24-25',
+    },
+    {
         text: 'Registration Info Sheet',
         url: 'https://utdirect.utexas.edu/registrar/ris.WBX',
     },
     {
-        text: 'Register For Courses',
+        text: 'Register for Courses',
         url: 'https://utdirect.utexas.edu/registration/chooseSemester.WBX',
     },
     {
         text: 'Degree Audit',
         url: 'https://utdirect.utexas.edu/apps/degree/audits/',
-    },
-    {
-        text: 'My Registered Courses',
-        url: 'https://utdirect.utexas.edu/registration/classlist.WBX',
     },
 ];
 
