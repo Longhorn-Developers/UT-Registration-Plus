a1f65a79894990026bb8e9ae022116e737092a71
suhas <hi@suhas.one>
2024-10-22 12:51:06 -0500

    fix: change types for storybook and add new story for CoreChip

--- src/stories/components/Chip.stories.tsx
+++ src/stories/components/Chip.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Chip } from '@views/components/common/Chip';
+import { Chip, coreMap, flagMap } from '@views/components/common/Chip';
 
 const meta = {
     title: 'Components/Common/Chip',
@@ -16,8 +16,18 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const FlagChip: Story = {
     args: {
         label: 'QR',
+        variant: 'flag',
+        labelMap: flagMap,
+    },
+};
+
+export const CoreChip: Story = {
+    args: {
+        label: 'SB',
+        variant: 'core',
+        labelMap: coreMap,
     },
 };
