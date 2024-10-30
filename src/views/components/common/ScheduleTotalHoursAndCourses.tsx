50b17fcdad3f2c7459f8e3bc35471aa63747ef0e
Warith Rahman <warithr21@gmail.com>
2024-10-30 11:20:45 -0500

    feat(header): added icons to cal header
    WORK IN PROGRESS

--- src/views/components/common/ScheduleTotalHoursAndCourses.tsx
+++ src/views/components/common/ScheduleTotalHoursAndCourses.tsx
@@ -1,6 +1,9 @@
 import Text from '@views/components/common/Text/Text';
 import React from 'react';
 
+import ClockIcon from '~icons/ic/baseline-access-time-filled';
+import BookIcon from '~icons/material-symbols/book-2-rounded';
+
 /**
  * Props for ScheduleTotalHoursAndCourses
  */
@@ -22,25 +25,27 @@ export default function ScheduleTotalHoursAndCourses({
 }: ScheduleTotalHoursAndCoursesProps): JSX.Element {
     return (
         <div className='min-w-full w-0 flex items-baseline gap-2.5 whitespace-nowrap'>
-            <Text className='truncate text-ut-burntorange uppercase' variant='h1' as='span'>
-                {`${scheduleName}: `}
+            <Text className='truncate text-ut-burntorange' variant='h1' as='span'>
+                {`${scheduleName} `}
             </Text>
             <div className='flex flex-row items-baseline gap-2'>
-                <div>
+                <div className='flex flex-row items-baseline gap-1'>
+                    <ClockIcon className='text-theme-black' style={{ fontSize: '2em' }} />
                     <Text variant='h1' as='span' className='font-normal normal-case!'>
                         <span className='text-theme-black'>{totalHours}</span>{' '}
                     </Text>
-                    <Text variant='h3' as='span' className='normal-case!'>
+                    <Text variant='h1' as='span' className='normal-case!'>
                         <span className='text-theme-black font-bold' style={{ fontVariant: 'all-small-caps' }}>
                             {totalHours === 1 ? 'HOUR' : 'HOURS'}
                         </span>
                     </Text>
                 </div>
-                <div>
+                <div className='flex flex-row items-baseline gap-1'>
+                    <BookIcon className='text-theme-black' style={{ fontSize: '2em' }} />
                     <Text variant='h1' as='span' className='font-normal normal-case!'>
                         <span className='text-theme-black'>{totalCourses}</span>{' '}
                     </Text>
-                    <Text variant='h3' as='span' className='normal-case!'>
+                    <Text variant='h1' as='span' className='normal-case!'>
                         <span className='text-theme-black font-bold' style={{ fontVariant: 'all-small-caps' }}>
                             {totalCourses === 1 ? 'COURSE' : 'COURSES'}
                         </span>
