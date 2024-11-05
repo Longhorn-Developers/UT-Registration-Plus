80b4cada1be472e4b610aefc796b26e9c67367f8
Ethan Lanting <ethanlanting@gmail.com>
2024-11-05 17:58:48 -0600

    refactor: enhance HexColorEditor to dynamically adjust tag icon color based on preview color

--- src/views/components/calendar/CalendarCourseCellColorPicker/HexColorEditor.tsx
+++ src/views/components/calendar/CalendarCourseCellColorPicker/HexColorEditor.tsx
@@ -1,5 +1,6 @@
-import { isValidHexColor } from '@shared/util/colors';
+import { isValidHexColor, pickFontColor } from '@shared/util/colors';
 import { getThemeColorHexByName } from '@shared/util/themeColors';
+import clsx from 'clsx';
 import React from 'react';
 
 import TagIcon from '~icons/material-symbols/tag';
@@ -24,6 +25,7 @@ export interface HexColorEditorProps {
 export default function HexColorEditor({ hexCode, setHexCode }: HexColorEditorProps): JSX.Element {
     const baseColor = React.useMemo(() => getThemeColorHexByName('ut-gray'), []);
     const previewColor = isValidHexColor(`#${hexCode}`) ? `#${hexCode}` : baseColor;
+    const tagColor = pickFontColor(previewColor.slice(1) as `#${string}`);
 
     return (
         <>
@@ -31,7 +33,7 @@ export default function HexColorEditor({ hexCode, setHexCode }: HexColorEditorPr
                 style={{ backgroundColor: previewColor }}
                 className='h-5.5 w-5.25 flex items-center justify-center rounded-l-1'
             >
-                <TagIcon className='h-4 w-4 text-color-white' />
+                <TagIcon className={clsx('h-4 w-4 text-color-white', tagColor)} />
             </div>
             <div className='h-5.5 w-[53px] flex flex-1 items-center justify-center border-b border-r border-t rounded-br rounded-tr p-1.25'>
                 <input
