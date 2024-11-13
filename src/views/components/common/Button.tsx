324f3534557eb86af54dde864820f63f0311ae72
Ethan Lanting <ethanlanting@gmail.com>
2024-11-12 19:52:42 -0600

    fix: revert Button component

--- src/views/components/common/Button.tsx
+++ src/views/components/common/Button.tsx
@@ -49,7 +49,7 @@ export function Button({
             className={clsx(
                 'btn',
                 {
-                    'text-white bg-opacity-100 hover:enabled:shadow-md active:enabled:shadow-sm shadow-black/20':
+                    '!text-white bg-opacity-100 hover:enabled:shadow-md active:enabled:shadow-sm shadow-black/20':
                         variant === 'filled',
                     'bg-opacity-0 border-current hover:enabled:bg-opacity-8 border': variant === 'outline',
                     'bg-opacity-0 border-none hover:enabled:bg-opacity-8': variant === 'single', // settings is the only "single"
