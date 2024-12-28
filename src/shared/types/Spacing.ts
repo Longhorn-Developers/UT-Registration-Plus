0753f0a72f6ea562f7431494d482c05fa7c3698d
Ethan <ethanlanting@gmail.com>
2024-12-27 20:33:03 -0600

    fix: update spacing constants to use rem units

--- src/shared/types/Spacing.ts
+++ src/shared/types/Spacing.ts
@@ -3,12 +3,12 @@
  * Each key corresponds to a specific spacing size based on the 4px grid system.
  */
 export const spacing = {
-    spacing0: '2px',
-    spacing1: '4px',
-    spacing2: '8px',
-    spacing3: '12px',
-    spacing4: '16px',
-    spacing5: '20px',
-    spacing6: '24px',
-    spacing7: '32px',
+    spacing0: '.125rem',
+    spacing1: '.25rem',
+    spacing2: '.5rem',
+    spacing3: '.75rem',
+    spacing4: '1rem',
+    spacing5: '1.25rem',
+    spacing6: '1.5rem',
+    spacing7: '2rem',
 } as const;
