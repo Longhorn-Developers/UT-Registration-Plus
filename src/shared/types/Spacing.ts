a7f8b77855d978c95d03a840b38cf71c19956bbb
Ethan <ethanlanting@gmail.com>
2024-12-30 23:33:47 -0600

    feat: add spacing system

---
+++ src/shared/types/Spacing.ts
@@ -0,0 +1,14 @@
+/**
+ * An object representing various spacing values used throughout the application.
+ * Each key corresponds to a specific spacing size based on the 4px grid system.
+ */
+export const spacing = {
+    spacing0: '.125rem',
+    spacing1: '.25rem',
+    spacing2: '.5rem',
+    spacing3: '.75rem',
+    spacing4: '1rem',
+    spacing5: '1.25rem',
+    spacing6: '1.5rem',
+    spacing7: '2rem',
+} as const;
