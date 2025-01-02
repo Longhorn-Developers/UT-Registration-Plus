d7351645d573dc22c5a4b41c5ef85de71eec6a24
doprz <52579214+doprz@users.noreply.github.com>
2025-01-01 22:08:41 -0600

    chore: add leading 0

--- src/shared/types/Spacing.ts
+++ src/shared/types/Spacing.ts
@@ -3,10 +3,10 @@
  * Each key corresponds to a specific spacing size based on the 4px grid system.
  */
 export const spacing = {
-    'spacing-1': '.125rem',
-    'spacing-2': '.25rem',
-    'spacing-3': '.5rem',
-    'spacing-4': '.75rem',
+    'spacing-1': '0.125rem',
+    'spacing-2': '0.25rem',
+    'spacing-3': '0.5rem',
+    'spacing-4': '0.75rem',
     'spacing-5': '1rem',
     'spacing-6': '1.25rem',
     'spacing-7': '1.5rem',
