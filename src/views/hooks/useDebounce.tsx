aa0402a23fe28acb64997ec0d9c0b608db9971d6
Ethan <ethanlanting@gmail.com>
2025-01-20 15:13:38 -0600

    refactor: rename SomeFunction to DebouncedCallback

--- src/views/hooks/useDebounce.tsx
+++ src/views/hooks/useDebounce.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useRef } from 'react';
 
 type Timer = ReturnType<typeof setTimeout>;
-type SomeFunction<T extends unknown[]> = (...args: T) => void;
+type DebouncedCallback<T extends unknown[]> = (...args: T) => void;
 
 /**
  * Custom hook to debounce a function call.
@@ -10,7 +10,7 @@ type SomeFunction<T extends unknown[]> = (...args: T) => void;
  * @param delay - The delay (in ms) for the function to return.
  * @returns The debounced function, which will run only if the debounced function has not been called in the last (delay) ms.
  */
-export function useDebounce<T extends unknown[]>(func: SomeFunction<T>, delay: number = 1000): SomeFunction<T> {
+export function useDebounce<T extends unknown[]>(func: DebouncedCallback<T>, delay: number = 1000): SomeFunction<T> {
     const timer = useRef<Timer>();
 
     useEffect(
