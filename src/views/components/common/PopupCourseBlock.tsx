e7b2dc028f1de97d16242b85a43dfdafa6370323
Ethan <ethanlanting@gmail.com>
2025-01-20 21:04:41 -0600

    feat: add copy functionality for course unique ID

--- src/views/components/common/PopupCourseBlock.tsx
+++ src/views/components/common/PopupCourseBlock.tsx
@@ -1,5 +1,5 @@
 import type { DraggableProvidedDragHandleProps } from '@hello-pangea/dnd';
-import { Copy, DotsSixVertical } from '@phosphor-icons/react';
+import { Check, Copy, DotsSixVertical } from '@phosphor-icons/react';
 import { background } from '@shared/messages';
 import { initSettings, OptionsStore } from '@shared/storage/OptionsStore';
 import type { Course } from '@shared/types/Course';
@@ -10,6 +10,8 @@ import { StatusIcon } from '@shared/util/icons';
 import Text from '@views/components/common/Text/Text';
 import clsx from 'clsx';
 import React, { useEffect, useRef, useState } from 'react';
+
+import { Button } from './Button';
 
 /**
  * Props for PopupCourseBlock
@@ -37,6 +39,8 @@ export default function PopupCourseBlock({
     dragHandleProps,
 }: PopupCourseBlockProps): JSX.Element {
     const [enableCourseStatusChips, setEnableCourseStatusChips] = useState<boolean>(false);
+    const [isCopied, setIsCopied] = useState(false);
+    const lastCopyTime = useRef<number>(0);
     const ref = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
@@ -72,13 +76,26 @@ export default function PopupCourseBlock({
         window.close();
     };
 
+    const handleCopy = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.stopPropagation();
+        const now = Date.now();
+        if (now - lastCopyTime.current < 1500) {
+            return;
+        }
+
+        lastCopyTime.current = now;
+        await navigator.clipboard.writeText(course.uniqueId.toString().padStart(5, '0'));
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 1500);
+    };
+
     return (
         <div
             style={{
                 backgroundColor: colors.primaryColor,
             }}
             className={clsx(
-                'h-full w-full inline-flex items-center justify-center gap-1 rounded pr-3 focusable cursor-pointer text-left hover:shadow-md ease-out group-[.is-dragging]:shadow-md',
+                'h-full w-full inline-flex items-center justify-center gap-1 rounded pr-2 focusable cursor-pointer text-left hover:shadow-md ease-out group-[.is-dragging]:shadow-md',
                 className
             )}
             onClick={handleClick}
@@ -93,12 +110,12 @@ export default function PopupCourseBlock({
             >
                 <DotsSixVertical weight='bold' className='h-6 w-6 text-white' />
             </div>
-            <Text className={clsx('flex-1 py-3.5 truncate', fontColor)} variant='h1-course'>
+            <Text className={clsx('flex-1 py-spacing-5 truncate', fontColor)} variant='h1-course'>
                 <span className='px-0.5 font-450'>{formattedUniqueId}</span> {course.department} {course.number}
                 {course.instructors.length > 0 ? <> &ndash; </> : ''}
                 {course.instructors.map(v => v.toString({ format: 'last' })).join('; ')}
             </Text>
-            {true && course.status !== Status.OPEN && (
+            {enableCourseStatusChips && course.status !== Status.OPEN && (
                 <div
                     style={{
                         backgroundColor: colors.secondaryColor,
@@ -109,15 +126,31 @@ export default function PopupCourseBlock({
                 </div>
             )}
 
-            <div
-                className='flex items-center justify-between gap-spacing-3 rounded px-spacing-3 py-spacing-2 text-white'
+            <Button
+                color='ut-gray'
+                onClick={handleCopy}
+                className='h-full max-h-[30px] w-fit gap-spacing-2 rounded py-spacing-2 text-white px-spacing-3!'
                 style={{
                     backgroundColor: colors.secondaryColor,
                 }}
             >
-                <Copy weight='fill' className='h-5.5 w-5.5 stroke-white text-white' />
-                <span className='font-medium'>{course.uniqueId}</span>
-            </div>
+                <div className='relative h-5.5 w-5.5'>
+                    <Check
+                        className={clsx(
+                            'absolute size-full  inset-0 text-white transition-all duration-250 ease-in-out',
+                            isCopied ? 'opacity-100 scale-100' : 'opacity-0 scale-75'
+                        )}
+                    />
+                    <Copy
+                        weight='fill'
+                        className={clsx(
+                            'absolute size-full inset-0 text-white transition-all duration-250 ease-in-out',
+                            isCopied ? 'opacity-0 scale-75' : 'opacity-100 scale-100'
+                        )}
+                    />
+                </div>
+                <span className='font-medium text-base!'>{course.uniqueId.toString().padStart(5, '0')}</span>
+            </Button>
         </div>
     );
 }
