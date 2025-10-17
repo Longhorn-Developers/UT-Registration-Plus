77170c110b3e87f5a0823efda9bfd92939d928f1
doprz <52579214+doprz@users.noreply.github.com>
2025-10-17 12:38:59 -0500

    refactor(nix): split into flake parts module and add shell.nix

--- flake.nix
+++ flake.nix
@@ -12,38 +12,8 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import systems;
       imports = [
-        inputs.treefmt-nix.flakeModule
+        ./nix/devShells.nix
+        ./nix/treefmt.nix
       ];
-
-      perSystem =
-        { pkgs, ... }:
-        let
-          commonPackages = with pkgs; [
-            nodejs_20 # v20.19.5
-            pnpm_10 # v10.18.0
-          ];
-
-          additionalPackages = with pkgs; [
-            bun
-            nodePackages.conventional-changelog-cli
-            sentry-cli
-          ];
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            name = "utrp-dev";
-            packages = commonPackages;
-          };
-
-          devShells.full = pkgs.mkShell {
-            name = "utrp-dev-full";
-            packages = commonPackages ++ additionalPackages;
-          };
-
-          treefmt = {
-            programs.nixfmt.enable = pkgs.lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.nixfmt-rfc-style.compiler;
-            programs.nixfmt.package = pkgs.nixfmt-rfc-style;
-          };
-        };
     };
 }
---
+++ nix/treefmt.nix
@@ -0,0 +1,18 @@
+{ inputs, ... }:
+{
+  imports = [
+    inputs.treefmt-nix.flakeModule
+  ];
+
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt = {
+        projectRootFile = "flake.nix";
+        programs.nixfmt.enable = pkgs.lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.nixfmt-rfc-style.compiler;
+        programs.nixfmt.package = pkgs.nixfmt-rfc-style;
+        programs.shellcheck.enable = true;
+        programs.yamlfmt.enable = true;
+      };
+    };
+}
