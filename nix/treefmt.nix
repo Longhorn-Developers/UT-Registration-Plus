a1fdfa9749ca32b17f2981d75784a9d0bd411af4
doprz <52579214+doprz@users.noreply.github.com>
2025-10-17 13:22:36 -0500

    chore: add prettier to treefmt and pin version

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -11,6 +11,7 @@
         projectRootFile = "flake.nix";
         programs.nixfmt.enable = pkgs.lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.nixfmt-rfc-style.compiler;
         programs.nixfmt.package = pkgs.nixfmt-rfc-style;
+        programs.prettier.enable = true;
         programs.shellcheck.enable = true;
         programs.yamlfmt.enable = true;
         programs.dockerfmt.enable = true;
