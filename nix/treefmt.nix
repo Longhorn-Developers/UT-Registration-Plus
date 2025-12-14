d12c8bd7b780f3611963b26b619918f38f804e1a
doprz <52579214+doprz@users.noreply.github.com>
2025-12-13 22:54:47 -0600

    chore: add treefmt/prettier note

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -11,6 +11,8 @@
         projectRootFile = "flake.nix";
         programs.nixfmt.enable = pkgs.lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.nixfmt-rfc-style.compiler;
         programs.nixfmt.package = pkgs.nixfmt-rfc-style;
+
+        # NOTE: Make sure the prettier version in package.json and the one used by treefmt are the same for consistent results
         programs.prettier.enable = true;
         programs.shellcheck.enable = true;
         programs.yamlfmt.enable = true;
