4776029cb4e45c313992a7835dba6c802ad94ca0
doprz <52579214+doprz@users.noreply.github.com>
2026-01-08 21:56:01 -0600

    fix: LHD bday

--- src/views/components/settings/constants.ts
+++ src/views/components/settings/constants.ts
@@ -7,7 +7,7 @@ export const DEV_MODE_CLICK_TARGET = 5;
 export const DEV_MODE_CLICK_TIMEOUT = 5000;
 export const DEV_MODE_CLICK_INTERVAL = 500;
 
-// LHD Birthday: January 9th, 2025
+// LHD Birthday: January 9th, 2024
 export const LHD_BIRTHDAY = { month: 0, day: 9 };
 export const BIRTHDAY_CELEBRATION_DURATION = 5000;
 export const BIRTHDAY_CELEBRATION_DEBOUNCE = 2000;
