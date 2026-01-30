1d7461d6a8a3c2ab39cece10a87017d3197c08ec
Derek Chen <derex1987@gmail.com>
2026-01-30 17:31:17 -0600

    chore: lint

--- src/views/components/settings/ContributorCardSkeleton.tsx
+++ src/views/components/settings/ContributorCardSkeleton.tsx
@@ -4,10 +4,10 @@ import React from 'react';
  * Lightweight skeleton placeholder for contributor cards while data loads
  */
 export const ContributorCardSkeleton: React.FC = () => (
-    <div className='border border-gray-300 rounded bg-ut-gray/10 p-4 animate-pulse'>
-        <div className='h-4 w-3/4 bg-gray-300 rounded mb-2' />
-        <div className='h-3 w-1/2 bg-gray-300 rounded mb-1' />
-        <div className='h-3 w-1/4 bg-gray-300 rounded' />
+    <div className='animate-pulse border border-gray-300 rounded bg-ut-gray/10 p-4'>
+        <div className='mb-2 h-4 w-3/4 rounded bg-gray-300' />
+        <div className='mb-1 h-3 w-1/2 rounded bg-gray-300' />
+        <div className='h-3 w-1/4 rounded bg-gray-300' />
     </div>
 );
 
