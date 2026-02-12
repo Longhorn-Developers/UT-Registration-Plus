1979dafe308ee04be28007d150f67d34a0479516
Derek <derex1987@gmail.com>
2026-02-11 23:18:17 -0600

    fix: messagelistener already defined

--- src/pages/calendar/CalendarMain.tsx
+++ src/pages/calendar/CalendarMain.tsx
@@ -7,7 +7,6 @@ import { MigrationDialog } from '@views/components/common/MigrationDialog';
 import { WhatsNewDialog } from '@views/components/common/WhatsNewPopup';
 import SentryProvider from '@views/contexts/SentryContext';
 import { UndoProvider } from '@views/contexts/UndoContext';
-import { MessageListener } from 'chrome-extension-toolkit';
 import useKC_DABR_WASM from 'kc-dabr-wasm';
 import React, { useEffect } from 'react';
 
