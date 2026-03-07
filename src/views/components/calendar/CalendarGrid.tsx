078ad9abc13360dfd7379808e1d1fd582a1aa04e
Derek <derex1987@gmail.com>
2026-03-07 04:48:43 -0600

    style: why was screenshot css class removed idk

--- src/views/components/calendar/CalendarGrid.tsx
+++ src/views/components/calendar/CalendarGrid.tsx
@@ -169,7 +169,7 @@ function AccountForCourseConflicts({ courseCells, setCourse }: AccountForCourseC
                         width: `calc(100% / ${block.totalColumns ?? 1})`,
                         marginLeft: `calc(100% * ${((block.gridColumnStart ?? 0) - 1) / (block.totalColumns ?? 1)})`,
                     }}
-                    className='pb-1 pl-0 pr-2.5 pt-0'
+                    className='pb-1 pl-0 pr-2.5 pt-0 screenshot:pb-0.5 screenshot:pr-0.5'
                 >
                     <CalendarCourseCell
                         courseDeptAndInstr={courseDeptAndInstr}
