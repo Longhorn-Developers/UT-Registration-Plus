53f1dd0848c98e6c0846ed2f1e0790c90d4b07f3
Razboy20 <razboy20@gmail.com>
2026-04-01 20:27:03 -0500

    fix: CES handler form-input & update to Fall 2025
    Fixes UTRP-171 and UTRP-144.

--- src/pages/background/handler/CESHandler.ts
+++ src/pages/background/handler/CESHandler.ts
@@ -2,37 +2,35 @@ import openNewTab from '@background/util/openNewTab';
 import type { MessageHandler } from '@chrome-extension-toolkit';
 import type CESMessage from '@shared/messages/CESMessage';
 
-const CESFall2023Url = 'https://utexas.bluera.com/utexas/rpvl.aspx?rid=d3db767b-049f-46c5-9a67-29c21c29c580&regl=en-US';
+const CESFall2025Url =
+    'https://my-utexas-bc.bluera.com/rpvlf.aspx?rid=2837da9d-d4e9-48a8-8af0-ecdc103dc254&regl=en-US&haslang=true';
 
 const CESHandler: MessageHandler<CESMessage> = {
-    openCESPage({ data, sendResponse }) {
+    async openCESPage({ data, sendResponse }) {
         const { instructorFirstName, instructorLastName } = data;
-        openNewTab(CESFall2023Url).then(tab => {
-            const instructorFirstAndLastName = [instructorFirstName, instructorLastName];
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                func: (...instructorFirstAndLastName: string[]) => {
-                    const inputElement = document.getElementById(
-                        'ctl00_ContentPlaceHolder1_ViewList_tbxValue'
-                    ) as HTMLInputElement | null;
-                    const [instructorFirstName, instructorLastName] = instructorFirstAndLastName;
-                    if (inputElement) {
-                        inputElement.value = `${instructorFirstName} ${instructorLastName}`;
-                        inputElement.focus();
-                        const enterKeyEvent = new KeyboardEvent('keydown', {
-                            key: 'Enter',
-                            code: 'Enter',
-                            keyCode: 13,
-                            which: 13,
-                            bubbles: true,
-                        });
-                        inputElement.dispatchEvent(enterKeyEvent);
-                    }
-                },
-                args: instructorFirstAndLastName,
-            });
-            sendResponse(tab);
+        const tab = await openNewTab(CESFall2025Url);
+        const instructorFirstAndLastName = [instructorFirstName, instructorLastName];
+        await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: (...instructorFirstAndLastName: string[]) => {
+                const inputElement = document.querySelector('input[type=text]') as HTMLInputElement | null;
+                const [instructorFirstName, instructorLastName] = instructorFirstAndLastName;
+                if (inputElement) {
+                    inputElement.value = `${instructorFirstName} ${instructorLastName}`;
+                    inputElement.focus();
+                    const enterKeyEvent = new KeyboardEvent('keydown', {
+                        key: 'Enter',
+                        code: 'Enter',
+                        keyCode: 13,
+                        which: 13,
+                        bubbles: true,
+                    });
+                    inputElement.dispatchEvent(enterKeyEvent);
+                }
+            },
+            args: instructorFirstAndLastName,
         });
+        sendResponse(tab);
     },
 };
 
