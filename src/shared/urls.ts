3e659c9e729bc8a27046b1e763205fe30c1f5356
Derek <derex1987@gmail.com>
2026-06-17 03:41:03 -0500

    docs: add jsdoc of where the uninstall repo is

--- src/shared/urls.ts
+++ src/shared/urls.ts
@@ -1,2 +1,7 @@
+/**
+ * The URL of the page users are redirected to after uninstalling the app.
+ *
+ * @see https://github.com/Longhorn-Developers/Uninstall-Page
+ */
 export const UNINSTALL_REVIEW_URL =
     'https://chromewebstore.google.com/detail/ut-registration-plus/hboadpjkoaieogjimneceaahlppnipaa/reviews?hl=en';
