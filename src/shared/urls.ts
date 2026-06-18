7bf0372b8d6bb2d84ac15562d6941b40eb5c5049
Derek <derex1987@gmail.com>
2026-06-17 23:55:29 -0500

    feat: change url

--- src/shared/urls.ts
+++ src/shared/urls.ts
@@ -4,4 +4,4 @@
  * @see https://github.com/Longhorn-Developers/Uninstall-Page
  */
 export const UNINSTALL_REVIEW_URL =
-    'https://chromewebstore.google.com/detail/ut-registration-plus/hboadpjkoaieogjimneceaahlppnipaa/reviews?hl=en';
+    'https://uninstall.longhorns.dev/';
