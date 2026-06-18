ee810ac6ffb21a4f66b83beba4d359d5161d9517
Derek Chen <derex1987@gmail.com>
2026-06-18 05:26:49 -0500

    Update background.ts

--- src/pages/background/background.ts
+++ src/pages/background/background.ts
@@ -51,9 +51,9 @@ if (import.meta.env.DEV) {
 }
 
 try {
-    chrome.runtime.setUninstallURL(UNINSTALL_REVIEW_URL);
+  await chrome.runtime.setUninstallURL(UNINSTALL_REVIEW_URL);
 } catch (e) {
-    console.error('Error opening uninstall page: ', e);
+  console.error('Error opening uninstall page: ', e);
 }
 
 setTraceContextProvider(() => {
