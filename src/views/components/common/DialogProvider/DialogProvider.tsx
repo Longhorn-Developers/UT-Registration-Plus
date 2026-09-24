e637ed470ab437fb865d6ae1ded18309dfb731eb
Derek Chen <derex1987@gmail.com>
2026-09-24 00:46:59 -0500

    feat: dirty merge
    * feat: adding setting to OptionsStore
    
    * feat: settings page
    
    * feat: dialog motion reduced
    
    ---------
    
    Co-authored-by: doprz <52579214+doprz@users.noreply.github.com>

--- src/views/components/common/DialogProvider/DialogProvider.tsx
+++ src/views/components/common/DialogProvider/DialogProvider.tsx
@@ -5,6 +5,8 @@ import { useCallback, useRef, useState } from 'react';
 
 import Dialog from '../Dialog';
 import Text from '../Text/Text';
+import { useEffect } from 'react';
+import { OptionsStore } from 'src/shared/storage/OptionsStore';
 
 type DialogElement = (show: boolean) => ReactNode;
 
